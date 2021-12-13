bf8b987da4abef3a5743f71a7d96cd10406a24ff
NoraLou <nora.humpage@gmail.com>
2021-12-13 12:18:22 -0500

    basic layout set-up

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -6,7 +6,12 @@ const PlayerLayout = ({ children }) => {
       <Box position="absolute" top="0" width="250px" left="0">
         Sidebar
       </Box>
-      <Box></Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        { children }
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
     </Box>
   );
 };
