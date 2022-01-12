ab7f90ae467bc4f691774ec0eb3bc9fd6cc6a5b8
NoraLou <nora.humpage@gmail.com>
2022-01-12 15:14:43 -0500

    fix small syntax error

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -52,7 +52,6 @@ const musicMenu = [
 const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 
 const Sidebar = () => {
-  const playlist =
   return (
     <Box
       width="100%"
