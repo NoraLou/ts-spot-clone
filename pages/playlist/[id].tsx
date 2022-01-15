f5e6c89b358790c364f8a35a72ee40c7c9af6a66
NoraLou <nora.humpage@gmail.com>
2022-01-14 20:48:02 -0500

    add in the songTable and format helpers

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,6 +1,7 @@
 import { validateToken } from '../../lib/auth'
 import prisma from '../../lib/prisma'
 import GradientLayout from '../../components/gradientLayout'
+import SongTable from '../../components/songsTable'
 
 const getBGColor = (id) => {
   const colors = [
@@ -28,7 +29,7 @@ const Playlist = ({ playlist }) => {
       description={`${playlist.songs.length} songs`}
       image={`https://picsum.photos/400?random=${playlist.id}`}
     >
-     <div></div>
+      <SongTable songs={playlist.songs} />
   </GradientLayout>
   )
 }
