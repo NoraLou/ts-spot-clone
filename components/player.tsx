b9466849a689fa3f2d57b0a37aa99a5952aa64ed
NoraLou <nora.humpage@gmail.com>
2022-01-19 19:32:22 -0500

    added in buttons and click handler functions for player UI

--- components/player.tsx
+++ components/player.tsx
@@ -7,7 +7,8 @@ import {
   RangeSliderThumb,
   Center,
   Flex,
-  Text
+  Text,
+  RangeSliderTrack
 } from '@chakra-ui/react'
 import ReactHowler from 'react-howler'
 import { useEffect, useRef, useState } from 'react'
@@ -22,11 +23,32 @@ import {
  } from 'react-icons/md'
 import { useStoreActions } from 'easy-peasy'
 
-const Player = () => {
+const Player = ({songs, activeSong }) => {
+  const [playing, setPlaying] = useState(true)
+  const [index, setIndex] = useState(0)
+  const [seek, setSeek] = useState(0.0)
+  const [repeat, setRepeat] = useState(false)
+  const [shuffle, setShuffle] = useState(false)
+  const [duration, setDuration] = useState(0.0)
+
+  const setPlayState = (value) => {
+    setPlaying(value)
+  }
+  // state is async so need use a callback function to get state value to update
+  const onShuffle = () => {
+    setShuffle((state) => !state)
+  }
+  const onRepeat = () => {
+    setRepeat((state) => !state)
+  }
+
   return (
     <Box>
       <Box>
-        {/* <ReactHowler /> */}
+        {/* <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+        /> */}
       </Box>
       <Center >
         <ButtonGroup>
@@ -35,7 +57,9 @@ const Player = () => {
             variant="link"
             aria-label="shuffle"
             fontSize="24px"
-            icon={ <MdShuffle />}
+            color={shuffle ? 'white' : 'gray.600'}
+            onClick={onShuffle}
+            icon={<MdShuffle />}
           />
           <IconButton
             outline="none"
@@ -44,22 +68,27 @@ const Player = () => {
             fontSize="24px"
             icon={<MdSkipPrevious/> }
           />
-          <IconButton
-            outline="none"
-            variant="link"
-            aria-label="play"
-            fontSize="40px"
-            color="white"
-            icon={ <MdPlayCircleFilled />}
-          />
-          <IconButton
-            outline="none"
-            variant="link"
-            aria-label="pause"
-            fontSize="40px"
-            color="white"
-            icon={ <MdOutlinePauseCircleFilled />}
-          />
+          { playing ? (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="pause"
+              fontSize="40px"
+              color="white"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={()=> setPlayState(false)}
+            />
+          ) : (
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="play"
+              fontSize="40px"
+              color="white"
+              icon={<MdPlayCircleFilled />}
+              onClick={()=> setPlayState(true)}
+            />
+          )}
           <IconButton
             outline="none"
             variant="link"
@@ -71,11 +100,37 @@ const Player = () => {
             outline="none"
             variant="link"
             aria-label="repeat"
+            color={repeat ? 'white' : 'gray.600'}
             fontSize="24px"
+            onClick={onRepeat}
             icon={ <MdOutlineRepeat />}
           />
         </ButtonGroup>
       </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={['min', 'max']}
+              step={0.1}
+              min={0}
+              max={321}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text font="xs">321</Text>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   )
 }
